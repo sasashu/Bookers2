ac26ef55898a6e7ac968d959e7a3242ed5340343
EC2 Default User <ec2-user@ip-172-31-38-185.ap-northeast-1.compute.internal>
2022-09-19 02:00:55 +0000

    [Add]User info作成中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,29 +1,55 @@
 class BooksController < ApplicationController
   
   def new
+    @book = Book.new
   end
   
   def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to book_path(@book.id), notice: 'Book was successfully created.'
+    else
+      @books = Book.all
+      render :index
+    end
   end
   
   def index
+    @books = Book.all
+    @book = Book.new
+    @user = current_user
   end
 
   def show
+    @book = Book.find(params[:id])
+    @user = @book.user
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
   
   def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+    redirect_to book_path(book.id),notice: 'Book was successfully updated.'
+    else
+      @books = Book.all
+      render :edit
+    end
   end
   
   def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path,alert: 'Book was successfully destroyed.'
   end
   
   private
   
   def book_params
+    params.require(:book).permit(:title,:body)
   end
   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          
-  has_many :book, dependent: :destroy
+  has_many :books, dependent: :destroy
   
   has_one_attached :profile_image
+  
+  validates :name, presence: true, length: { minimum: 2, maximum: 20}
+  validates :introduction, length: { maximum: 50 }
+
 
 end
