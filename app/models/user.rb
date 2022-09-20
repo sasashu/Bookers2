e040afb4f6e50348abae76bbf614c128e062db65
EC2 Default User <ec2-user@ip-172-31-38-185.ap-northeast-1.compute.internal>
2022-09-20 08:38:32 +0000

    [Add]RSpec導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,6 @@ class User < ApplicationRecord
   
   has_one_attached :profile_image
   
-  validates :name, presence: true, length: { minimum: 2, maximum: 20}
+  validates :name, presence: true, uniqueness: { case_sensitive: false } ,length: { minimum: 2, maximum: 20}
   validates :introduction, length: { maximum: 50 }
-
-
 end
